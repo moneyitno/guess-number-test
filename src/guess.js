b4487505088044d8bc54a24e788e2cdcf19086c4
your_name <your_email>
2014-07-25 09:32:07 +0800

    guess count less than 7

--- src/guess.js
+++ src/guess.js
@@ -1,3 +1,20 @@
-guessCountBy = function (){
+guessCountBy = function () {
+    var count = JSON.parse(localStorage.getItem('countNumber')) || 0;
+    var printNumber = getNumber();
+    if (count < 6) {
+        if (printNumber) {
+            contrast(printNumber, produceRandomNumber());
+            count = count + 1;
+            return count;
+        }
+    }
+
+};
+
+getNumber = function () {
+    return [1, 2, 3, 4]
+};
+
+contrast = function () {
 
 };
